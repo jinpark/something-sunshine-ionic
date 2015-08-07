bae1a5be6f1376c980a2b60c65c7a79248a9efe9
Jin Park <jin+github@jinpark.net>
2015-08-07 21:37:24 +0900

    try some audio stuff, stuck with ngaudio, added a spinner for loading

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -15,7 +15,6 @@ angular.module('starter.controllers', ['ngAudio'])
     $http.get('http://www.somethingsunshine.com/api/episodes').then(function(data){
         $ionicLoading.hide();
         $scope.episodes = data.data;
-        console.log($scope.episodes);
     }, function(){
         $ionicLoading.show({template: 'Something went wrong...', duration: 2000});
     })
@@ -23,14 +22,35 @@ angular.module('starter.controllers', ['ngAudio'])
 })
 
 .controller('EpisodeCtrl', function($scope, $stateParams, $window, $http, $ionicLoading, ngAudio) {
+    $scope.audio = null;
+    $scope.loading = true;
+    $scope.seeking = false;
     $ionicLoading.show({template: 'Loading'});
     $http.get('http://www.somethingsunshine.com/api/episode/' + $stateParams.episodeId).then(function(data){
+        $scope.loading = false;
         $ionicLoading.hide();
         $scope.episode = data.data[0];
         $scope.audio = ngAudio.load($scope.episode.fields.audio_file_path);
+        console.log($scope.audio.currentTime);
     }, function(){
+        $scope.loading = false;
         $ionicLoading.show({template: 'Something went wrong...', duration: 2000});
     })
+    $scope.$watch('audio.audio.seeking', function(oldVal, newVal){
+        $scope.seeking = false;
+        console.log(['watching', oldVal, newVal]);
+        if (oldVal == true && newVal == false) {
+            $scope.seeking = true;
+            console.log('seeking');
+        } else {
+             $scope.seeking = false;
+        }
+    });
+    $scope.$on('$ionicView.leave', function(){
+      if ($scope.audio){
+        $scope.audio.stop();
+      }
+    });
 })
 
 .directive('backImg', function(){
@@ -44,7 +64,7 @@ angular.module('starter.controllers', ['ngAudio'])
         };
 })
 .directive('seekbar', function($window){
-    var template = '<input class="form-control seekbar" type="range" min="0" max="1" step="0.01">'
+    var template = '<input class="form-control seekbar" type="range" min="0" max="1" step="0.01" value="0">'
     var getTrackStyle = function (el) {
         var curVal = el.val() * 100,
             style = {},
